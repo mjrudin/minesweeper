7bea858734e9bba079b95cc50a766e1af859fecd
Ethan Dickinson and Matt Rudin <ethan+matt@appacademy.io>
2013-06-24 16:17:31 -0400

    Finished program, not refactored

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
+require "yaml"
 class BackgroundBoard
 
   attr_accessor :grid
 
-  def initialize(ui)
+  def initialize(ui, mode)
     @ui = ui
     @grid = []
-    9.times { @grid << Array.new(9, nil) }
-    mine_setup
+    build_grid(mode)
+  end
+
+  def build_grid(mode)
+    mode[1].times { @grid << Array.new(mode[0], nil) }
+    mine_setup(mode)
     fringe_setup
   end
 
@@ -24,7 +29,7 @@ class BackgroundBoard
         (coordinates[1] - 1).upto(coordinates[1] + 1) do |j|
           next if j < 0 || j > (@grid.count - 1)
 
-          reveal([i,j]) if @ui.user_grid[i][j] == "*"
+          reveal([i,j]) if @ui.user_grid[i][j] == "🐨"
         end
       end
     else
@@ -32,11 +37,11 @@ class BackgroundBoard
     end
   end
 
-  def mine_setup
-    10.times do
+  def mine_setup(mode)
+    mode[2].times do
       bomb_space = 0
       while bomb_space do
-        row, col = rand(9), rand(9)
+        row, col = rand(mode[1]), rand(mode[0])
         bomb_space = @grid[row][col]
       end
       @grid[row][col] = :bomb
@@ -76,12 +81,20 @@ class UI
 
   def initialize
     @user_grid = []
-    9.times { @user_grid << Array.new(9, "*") }
-    @board = BackgroundBoard.new(self)
   end
 
   def run
     puts "Welcome to MineSweeper!"
+    puts "Enter game mode: easy, medium, or expert"
+    mode_hash = {"easy" => [9,9,10],
+                 "medium" => [16,16,40],
+                 "expert" => [30,16,99]}
+
+    @mode = mode_hash[gets.downcase.strip]
+
+    build_grid
+    @board = BackgroundBoard.new(self, @mode)
+
     display_user_board
 
     until done?
@@ -93,23 +106,33 @@ class UI
     nil
   end
 
+  def build_grid
+    @mode[1].times { @user_grid << Array.new(@mode[0], "🐨") }
+  end
+
   def prompt_user
     valid_entry = nil
     until valid_entry
-        puts "Input \"r\" to reveal or \"f\" to flag, " +
-             "followed by coordinates (e.g. r 2 5)."
-         valid_entry = gets_entry
+      puts "Input \"r\" to reveal or \"f\" to flag, " +
+           "followed by coordinates (e.g. r 2 5).\n"  +
+           "Or type \"load\" to load a game file or \"save\" to save."
+      valid_entry = gets_entry
     end
     valid_entry
   end
 
   def gets_entry
     entry = gets.chomp.downcase.split(/\s+/)
+    return "save" if entry[0] == "save"
+    return "load" if entry[0] == "load"
+
     if ["r","f"].include?(entry[0])
-      if ("0".."8").cover?(entry[1]) && ("0".."8").cover?(entry[2])
+      if ("0"..((@mode[0]-1).to_s)).include?(entry[1]) &&
+         ("0"..((@mode[1]-1).to_s)).include?(entry[2])
         return entry
       else
-        puts "Make sure coordinates are between 0 and 8."
+        puts "Make sure coordinates are between 0 and #{@mode[0]} or " +
+              "0 and #{@mode[1]}."
       end
     else
       puts "Precede your coordinates with \"r\" to reveal or \"f\" to flag."
@@ -118,12 +141,14 @@ class UI
   end
 
   def execute_entry(valid_entry)
+    return save if valid_entry == "save"
+    return load if valid_entry == "load"
     command = valid_entry[0]
     coordinates = valid_entry[1..2].reverse.map(&:to_i)
     if command[0] == "r"
-      if @user_grid[coordinates[0]][coordinates[1]] == "F"
+      if @user_grid[coordinates[0]][coordinates[1]] == "📮"
         puts "You must unflag this space before revealing it."
-        prompt_user
+        execute_entry(prompt_user)
       else
         @board.reveal(coordinates)
       end
@@ -133,15 +158,15 @@ class UI
   end
 
   def flag(coordinates)
-    if @user_grid[coordinates[0]][coordinates[1]] == "F"
-      @user_grid[coordinates[0]][coordinates[1]] = "*"
+    if @user_grid[coordinates[0]][coordinates[1]] == "📮"
+      @user_grid[coordinates[0]][coordinates[1]] = "🐨"
     else
-      @user_grid[coordinates[0]][coordinates[1]] = "F"
+      @user_grid[coordinates[0]][coordinates[1]] = "📮"
     end
   end
 
   def display_bomb(coords)
-    @user_grid[coords[0]][coords[1]] = "©"
+    @user_grid[coords[0]][coords[1]] = "💣"
   end
 
   def add_number(coords)
@@ -153,9 +178,12 @@ class UI
   end
 
   def display_user_board
-    puts "  " + (0..8).to_a.join(" ")
+    puts "   " + (0...9).to_a.join("  ") + "   " +
+         (9...@user_grid[0].count).to_a.join(" ")
+
     @user_grid.each_with_index do |row, index|
-      puts "#{index} #{row.join(" ")}"
+      puts " #{index} #{row.join("  ")}" if index < 10
+      puts "#{index} #{row.join("  ")}" if index >= 10
     end
   end
 
@@ -167,14 +195,14 @@ class UI
     unexpored_spaces = 0
     @user_grid.each do |row|
       row.each do |space|
-        unexpored_spaces += 1 if space == "*" || space == "F"
+        unexpored_spaces += 1 if space == "🐨" || space == "📮"
       end
     end
-    unexpored_spaces == 10
+    unexpored_spaces == @mode[2]
   end
 
   def lost?
-    @user_grid.any?{|row| row.any?{|space| space == "©"}}
+    @user_grid.any?{|row| row.any?{|space| space == "💣"}}
   end
 
   def give_results
@@ -192,6 +220,21 @@ class UI
         @board.reveal([index1, index2]) if space == :bomb
       end
     end
+  end
+
+  def save
+    gamestate = [@board.grid, user_grid].to_yaml
+    puts "You're saving your current game. Enter a file name (no extension):"
+    filename = gets.chomp.downcase
+    File.open("#{filename}.txt", "w") {|f| f.puts gamestate}
+  end
+
+  def load
+    puts "Enter a filename from this directory (no extension):"
+    filename = gets.chomp.downcase
+    loaded_file = File.read("#{filename}.txt")
+    object_array = YAML::load(loaded_file)
+    @board.grid, @user_grid = object_array[0], object_array[1]
   end
 end
 #for running from command line, not in irb
